6dc21a7aa0fa209c34442712a19915bc8a78f8fb
nehak6296 <nehak6296@gmail.com>
2021-03-23 20:34:34 +0530

    [Neha]Add:Stack is created

--- StackAndQueueDemo/StackAndQueueDemo/Program.cs
+++ StackAndQueueDemo/StackAndQueueDemo/Program.cs
@@ -8,6 +8,11 @@ namespace StackAndQueueDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stack And Queue !");
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
         }
     }
 }
