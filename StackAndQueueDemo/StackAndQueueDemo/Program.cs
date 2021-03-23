1ecc1e941ea184e9b7b0c203775dd2550a7824a8
nehak6296 <nehak6296@gmail.com>
2021-03-23 20:00:21 +0530

    [Neha]Added: Welcome Message

---
+++ StackAndQueueDemo/StackAndQueueDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace StackAndQueueDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack And Queue !");
+        }
+    }
+}
